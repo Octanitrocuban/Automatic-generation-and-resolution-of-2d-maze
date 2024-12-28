3f7721eb5a7145c5f86eea55dde243a9d38e4af9
Octanitrocuban <113181068+Octanitrocuban@users.noreply.github.com>
2024-12-28 13:40:43 +0100

    Add files via upload

--- src/examples.py
+++ src/examples.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 """
-Script to test the different functions of maze_generator, maze_solver.py and
-maze_imshow.py.
+Script to test the different functions of maze_generator.py, maze_solver.py
+and maze_imshow.py.
 """
+
 import numpy as np
 from tqdm import tqdm
 from time import time
@@ -19,11 +20,11 @@ to_do = ['maze_test']
 if 'maze_test' in to_do:
 	# int : size of the maze, between 2 and +inf (or at least as much as your
 	# computer can handle it)
-	size = 51
+	size = 201
 
-	# str : algorithm used to create the maze ['fusion', 'randwalk', 'kurskal'
-	# or 'ticking']
-	creation = 'randwalk'
+	# str : algorithm used to create the maze ['fusion', 'randwalk',
+	# 'kurskal', 'ticking' or 'jumper']
+	creation = 'jumper'
 
 	# bool : if some walls are broke to create loop and thus a not having an
 	# unique solution to the maze.
@@ -41,25 +42,24 @@ if 'maze_test' in to_do:
 
 	mi.full_maze(size, creation, complexit, resolve, plot, timing)
 
-
 if 'maze_stats' in to_do:
-	# Sizes of the mazes
-	widths = np.arange(11, 82, 10)
+	# Sizes of the mazes [11, 21, 31, ..., 71, 81]
+	widths = np.arange(11, 102, 10)
 
 	# Number of creation of the same type and size of maze
 	n_runs = 1000
 
-	# to store the connectivity stats
-	empty = np.zeros((len(widths), n_runs, 4, 4))
-	empty_runed = np.load('connections.npy', allow_pickle=True)
-	empty[:, :, :4] = empty_runed
+	# to store the connectivity stats (n sizes, n_runs, type, caracte)
+	empty = np.zeros((len(widths), n_runs, 5, 4))
 
 	# to store the speed of creation of maze
-	timer = np.zeros((len(widths), n_runs, 4))
-	timer_runed = np.load('timer.npy', allow_pickle=True)
-	timer[:, :, :4] = timer_runed
+	timer = np.zeros((len(widths), n_runs, 5))
 
 	for i in range(len(widths)):
+		mesh = np.meshgrid(np.arange(1, widths[i], 2),
+						   np.arange(1, widths[i], 2))
+
+		mesh = np.array([np.ravel(mesh[1]), np.ravel(mesh[0])]).T
 		for r in tqdm(range(n_runs)):
 			t0 = time()
 			fusion = mg.maze_formation(mg.create_maze_base(widths[i]))
@@ -79,10 +79,17 @@ if 'maze_stats' in to_do:
 			ticktack = mg.ticking_maze(widths[i])
 			timer[i, r, 3] = time()-t0
 
-			empty[i, r, 0] = mi.caracterisation(fusion)
-			empty[i, r, 1] = mi.caracterisation(rand_walk)
-			empty[i, r, 2] = mi.caracterisation(min_stree)
-			empty[i, r, 3] = mi.caracterisation(ticktack)
+			t0 = time()
+			jumper = mg.jumping_explorer(
+							mg.create_maze_base_boolean(widths[i]))
+
+			timer[i, r, 4] = time()-t0
+
+			empty[i, r, 0] = mi.caracterisation(fusion, mesh)
+			empty[i, r, 1] = mi.caracterisation(rand_walk, mesh)
+			empty[i, r, 2] = mi.caracterisation(min_stree, mesh)
+			empty[i, r, 3] = mi.caracterisation(ticktack, mesh)
+			empty[i, r, 4] = mi.caracterisation(jumper, mesh)
 
 	empty = np.round(empty, 12).astype('float32')
 	timer = np.round(timer, 12).astype('float32')
@@ -157,27 +164,44 @@ if 'maze_stats' in to_do:
 			else:
 				parts[l].set_color('orange')
 
+		plt.plot(positions_loc, np.median(empty[:, :, 4, nod], axis=1), symb,
+				 color='grey', label='jumper', markersize=mksz)
+
+		parts = plt.violinplot(empty[:, :, 4, nod].T, positions_loc)
+		for l in parts:
+			if type(parts[l]) == list:
+				for pieace in parts[l]:
+					pieace.set_color('grey')
+					pieace.set_zorder(3)
+
+			else:
+				parts[l].set_color('grey')
+
 		plt.xticks(positions_loc, widths, fontsize=tksz)
 		plt.yticks(fontsize=tksz)
-		leg = plt.legend(title='Methods (median)', fontsize=lbsz,
-						 title_fontsize=lbsz, markerscale=4, ncol=4)
+		if nod == 1:
+			leg = plt.legend(title='Methods (median)', fontsize=lbsz,
+							 title_fontsize=lbsz, markerscale=4, ncol=3,
+							 loc='lower right')
+
+		else:
+			leg = plt.legend(title='Methods (median)', fontsize=lbsz,
+							 title_fontsize=lbsz, markerscale=4, ncol=3)
 
 		for lin in leg.get_lines():
 			lin.set_linewidth(3)
 
 		plt.xlabel('Width of the maze', fontsize=lbsz)
 		plt.ylabel('PDF (nodes/tot nodes)', fontsize=lbsz)
-		plt.savefig('distribution_of_connections_'+str(nod+1)+'.png',
+		plt.savefig('../img/distribution_of_connections_'+str(nod+1)+'.png',
 					bbox_inches='tight')
 
 		plt.close()
 		plt.show()
 
-
 	# figure for the time took to create the mazes
 	plt.figure(figsize=(20, 10))
 	plt.title('Time consumption', fontsize=lbsz)
-
 	plt.grid(True, zorder=1)
 	plt.plot(positions_loc, np.median(timer[:, :, 0], axis=1), symb,
 			 color='b', label='fusion', markersize=mksz)
@@ -231,10 +255,23 @@ if 'maze_stats' in to_do:
 		else:
 			parts[l].set_color('orange')
 
+	plt.plot(positions_loc, np.median(timer[:, :, 4], axis=1), symb,
+			 color='grey', label='jumper', markersize=mksz)
+
+	parts = plt.violinplot(timer[:, :, 4].T, positions_loc)
+	for l in parts:
+		if type(parts[l]) == list:
+			for pieace in parts[l]:
+				pieace.set_color('grey')
+				pieace.set_zorder(3)
+
+		else:
+			parts[l].set_color('grey')
+
 	plt.xticks(positions_loc, widths, fontsize=tksz)
 	plt.yticks(fontsize=tksz)
 	leg = plt.legend(title='Methods (median)', fontsize=lbsz,
-					 title_fontsize=lbsz, markerscale=4, ncol=4,
+					 title_fontsize=lbsz, markerscale=4, ncol=3,
 					 loc='upper left')
 
 	for lin in leg.get_lines():
@@ -242,9 +279,6 @@ if 'maze_stats' in to_do:
 
 	plt.xlabel('Width of the maze', fontsize=lbsz)
 	plt.ylabel('Time (seconds)', fontsize=lbsz)
-	plt.savefig('time_contruction_methods.png', bbox_inches='tight')
+	plt.savefig('../img/time_contruction_methods.png', bbox_inches='tight')
 	plt.close()
 	plt.show()
-
-	np.save('connections.npy', empty)
-	np.save('timer.npy', timer)
