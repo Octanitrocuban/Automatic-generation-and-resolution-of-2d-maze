f6d9a8d7c78f681f93378bf4a7a0e2d8f091400c
Octanitrocuban <113181068+Octanitrocuban@users.noreply.github.com>
2025-10-17 15:28:51 +0200

    Add files via upload

--- src/examples.py
+++ src/examples.py
@@ -1,12 +1,10 @@
 # -*- coding: utf-8 -*-
 """
-Script to test the different functions of maze_generator.py, maze_solver.py
-and maze_imshow.py.
+Script to test the different functions generate and solve mazes.
 """
 
 import numpy as np
 import maze_imshow as mi
-
 #=============================================================================
 
 # int : size of the maze, between 2 and +inf (or at least as much as your
@@ -14,14 +12,14 @@ import maze_imshow as mi
 size = 51
 
 # str : algorithm used to create the maze ['fusion', 'randwalk',
-# 'kurskal', 'ticking_f', 'ticking_s', 'jumper', 'hunter', 'grower',
+# 'kruskal', 'oshift_f', 'oshift_s', 'jumper', 'hunter', 'grower',
 # 'Eller', 'sidewinder', 'bintree', 'bintree_sn', 'bintree_ce',
 # 'bintree_rc', 'bintree_sp']
 creation = 'bintree_sp'
 
 # bool : if some walls are broke to create loop and thus a not having an
 # unique solution to the maze.
-complexit = False
+multipath = False
 
 # list of str : from ('pre_reduc' or None) and ('right_hand',
 # 'right_hand_single', 'left_hand', 'left_hand_single', or 'straight')
@@ -33,4 +31,4 @@ plot = 'all'
 # bool : print (or not) the time for the execution of the tasks.
 timing = False
 
-maze = mi.full_maze(size, creation, complexit, resolve, plot, timing)
+maze = mi.full_maze(size, creation, multipath, resolve, plot, timing)
